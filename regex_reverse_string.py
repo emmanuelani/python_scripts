3ad54c99a304133ef2a7213a2e1b5ff1cb0af782
Emmanuel <emmanuelani234@gmail.com>
2022-05-31 14:06:40 +0100

    new python script

--- regex_reverse_string.py
+++ regex_reverse_string.py
@@ -4,16 +4,15 @@ import re
 
 # Creating a function to do the job
 def reverse_string(string):
-    splitted_string = string.split(' ')
-    print(splitted_string)
     # This assert function would ensure that the words in the string are not more than 5
     # this assert statement is to ensure consistency in capturing the groups in the string
-    assert len(splitted_string) == 5, 'The words in the string should not be more than 5'
+    assert len(string.split(' ')) == 5, 'The words in the string should not be more than 5'
     # Making sure the string is not alphanumeric
     assert not string.isnumeric(), 'The string shouldn\'t contain any numeric value'
     # Writing out the regular expression that would be used to match the string and capture groups
     regex = r'^(\w*) (\w*) (\w*) (\w*) (\w*)$'
     result = re.search(regex, string)
+    print(result)
     # Reversing the string
     reversed_string = "{} {} {} {} {}".format(result[5], result[4], result[3], 
                                               result[2], result[1])
@@ -21,4 +20,8 @@ def reverse_string(string):
 
 reverse_string("My name is Emmanuel Ani")
     
+
+string = "Hello World Hello"
+assert len(string.split(' ')) == 4, 'Number of words in string should not be more than 4'
+print(string)
     
